ad761af1c661130c378a4bccd62b2bb78119b1c7
takatsugu-kato <t.kato@crestecusa.com>
2021-03-31 11:20:35 +0900

    fix g tag nest issue

--- libtrados/sdlxliff.py
+++ libtrados/sdlxliff.py
@@ -54,8 +54,9 @@ class Sdlxliff():
                 trans_unit_obj.source = self.__create_seg_obj(trans_unit_element, "source")
                 trans_unit_obj.set_only_tag_flag()
 
-                trans_unit_obj.seg_source = self.__create_seg_mrk(trans_unit_element.find('xliff:seg-source', self.namespace))
-                trans_unit_obj.target = self.__create_seg_mrk(trans_unit_element.find('xliff:target', self.namespace))
+                if not trans_unit_obj.only_tag:
+                    trans_unit_obj.seg_source = self.__create_seg_mrk(trans_unit_element.find('xliff:seg-source', self.namespace))
+                    trans_unit_obj.target = self.__create_seg_mrk(trans_unit_element.find('xliff:target', self.namespace))
 
                 # セグメントがMT由来かどうかはここで取得する<sdl:seg-defs><sdl:seg id="3" conf="Draft" origin="mt" origin-system="Google Cloud Translation API"/>
                 trans_unit_count = trans_unit_count + 1
@@ -87,17 +88,8 @@ class Sdlxliff():
         mrk_elements = seg_element.findall('.//xliff:mrk', self.namespace)
         mrks = []
         for mrk_element in mrk_elements:
-            mrk_obj = Mrk(mrk_element.get('mid'))
-            mrk_obj.string = mrk_element.text
-            if mrk_obj.string is None:
-                g_objects = []
-                g_elements = mrk_element.findall('xliff:g', self.namespace)
-                for g_element in g_elements:
-                    g_obj = GObj(g_element.get('id'), g_element.get('xid'))
-                    g_obj.string = g_element.text.strip()
-                    g_objects.append(g_obj)
-                mrk_obj.g_objs = g_objects
-
+            mrk_obj = Mrk(mrk_element.get('mid'), etree.tostring(mrk_element), mrk_element.tail)
+            mrk_obj.string = self.__delete_element_tag(etree.tostring(mrk_element))
             mrks.append(mrk_obj)
 
         return mrks
@@ -116,35 +108,34 @@ class Sdlxliff():
         string = etree.tostring(element, encoding='unicode')
         return self.__clean_element_string(string)
 
-    def back_to_xlf(self):
+    def back_to_xlf(self, out_path=None):
+        """Generate to xlf file from File object
+
+        Args:
+            out_path (str, optional): Out put file path. Defaults to ovierwrite original file.
         """
-        Generate to xlf file from File object
-        """
+
+        if out_path is None:
+            out_path = self.path
 
         for file in self.files:
             for trans_unit in file.trans_units:
 
                 for target in trans_unit.target:
-                    if target.string is None:
-                        for g_obj in target.g_objs:
-                            new_g_element = self.__create_xml_string_for_g_element(g_obj)
-                            condition = (
-                                'xliff:file[@original="{0}"]/xliff:body/xliff:group/xliff:trans-unit[@id="{1}"]/xliff:target//xliff:mrk[@mid="{2}"]/xliff:g[@id="{3}"]'
-                                .format(file.original, trans_unit.trans_unit_id, target.mid, g_obj.gid))
-                            g_element = self.root.find(condition, self.namespace)
-                            parent = g_element.getparent()
-                            parent.remove(g_element)
-                            parent.append(new_g_element)
-                    else:
-                        new_mrk_element = self.__create_xml_string_for_mrk_element(target)
-                        condition = (
-                            'xliff:file[@original="{0}"]/xliff:body/xliff:group/xliff:trans-unit[@id="{1}"]/xliff:target//xliff:mrk[@mid="{2}"]'
-                            .format(file.original, trans_unit.trans_unit_id, target.mid))
-                        mrk_element = self.root.find(condition, self.namespace)
-                        parent = mrk_element.getparent()
-                        parent.remove(mrk_element)
-                        parent.append(new_mrk_element)
-        self.tree.write(self.path, encoding="utf-8", xml_declaration=True)
+                    new_mrk_element = self.__create_xml_string_for_mrk_element(target)
+                    condition = (
+                        'xliff:file[@original="{0}"]/xliff:body/xliff:group/xliff:trans-unit[@id="{1}"]/xliff:target//xliff:mrk[@mid="{2}"]'
+                        .format(file.original, trans_unit.trans_unit_id, target.mid))
+                    mrk_element = self.root.find(condition, self.namespace)
+                    parent = mrk_element.getparent()
+                    parent.replace(mrk_element, new_mrk_element)
+        self.tree.write(out_path, encoding="utf-8", xml_declaration=True)
+
+    @staticmethod
+    def __delete_element_tag(string):
+        string = string.decode()
+        string = re.sub('<mrk.*?>(.*?)</mrk>.*', r"\1", string, flags=re.DOTALL)
+        return string
 
     @staticmethod
     def __clean_element_string(string):
@@ -174,24 +165,7 @@ class Sdlxliff():
         """
         xml_string = '<mrk mtype="seg" mid="{0}">{1}</mrk>'.format(mrk_obj.mid, mrk_obj.string)
         tree = etree.fromstring(xml_string)
-        return tree
-
-    @staticmethod
-    def __create_xml_string_for_g_element(g_obj):
-        """
-        Create xml string for Mrk element
-
-        Args:
-            g_obj (G_OBJ): Mrk object in this class
-
-        Returns:
-            str: xml string
-        """
-        if g_obj.xid:
-            xml_string = '<g id="{0}" xid="{1}">{2}</g>'.format(g_obj.gid, g_obj.xid, g_obj.string)
-        else:
-            xml_string = '<g id="{0}">{1}</g>'.format(g_obj.gid, g_obj.string)
-        tree = etree.fromstring(xml_string)
+        tree.tail = mrk_obj.tail
         return tree
 
 class File():
@@ -222,17 +196,7 @@ class TransUnit():
         """
         Set only_tag flag using source string
         """
-        source_temp = self.source.string
-        # delete format tags
-        repatter = re.compile(r'{([0-9]+)&gt;(.*?)&lt;\1}')
-        while repatter.search(source_temp):
-            source_temp = repatter.sub('\\2', source_temp, count=1)
-
-        # delete placeholder tags
-        repatter = re.compile(r'{([0-9]+)}')
-        source_temp = repatter.sub("", source_temp)
-        source_temp = source_temp.rstrip()
-        if source_temp == "":
+        if self.source.string == "":
             self.only_tag = True
 
 
@@ -248,16 +212,7 @@ class Mrk():
     """
     Object of <mrk>
     """
-    def __init__(self, mid):
+    def __init__(self, mid, string, tail):
         self.mid = mid
-        self.string = ""
-        self.g_objs = []
-
-class GObj():
-    """
-    Object of <g>
-    """
-    def __init__(self, gid, xid):
-        self.gid = gid
-        self.xid = xid
-        self.string = ""
+        self.string = string
+        self.tail = tail
